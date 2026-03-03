ab898c36b645bdc96763b397b138a99181a5cf04
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2026-03-03 17:41:46 -0600

    feat: setting up config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,40 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  experimental: {
+    fonts: [
+      {
+        name: "Oswald",
+        cssVariable: "--font-oswald",
+        provider: fontProviders.google(),
+      },
+      {
+        name: "Lato",
+        cssVariable: "--font-lato",
+        provider: fontProviders.google(),
+      },
+      {
+        name: "Cutive Mono",
+        cssVariable: "--font-cutive-mono",
+        provider: fontProviders.google(),
+      },
+      {
+        name: "Abril Fatface",
+        cssVariable: "--font-abril-fatface",
+        provider: fontProviders.google(),
+      },
+    ],
+  },
+
+  integrations: [icon()],
+});
